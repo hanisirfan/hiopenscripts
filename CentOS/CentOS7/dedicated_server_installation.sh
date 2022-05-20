01269c13d497fa1cf09a7041964fb76920b0268d
Muhammad Hanis Irfan <hanisirfan.work@gmail.com>
2022-05-20 17:56:37 +0800

    Increase interface range for Additional IPs

--- CentOS/CentOS7/dedicated_server_installation.sh
+++ CentOS/CentOS7/dedicated_server_installation.sh
@@ -6,7 +6,7 @@
 # Interactive Menu based on: https://gist.github.com/shinokada/9d54c820b127d0b771c3d87a157fc99d
 ###############################################################################################
 
-scriptversion=0.1.1
+scriptversion=0.1.2
 
 ### Message and logs ###
 CURRENT_DATE () {
@@ -148,7 +148,7 @@ addadditionalip() {
         # Just hope that the admin inserted the IP and prefix correctly.
         read -r -p "IP Address (x.x.x.x): " ipaddress
         read -r -p "IP Prefix (1-32): " ipprefix
-        for i in {2..10}
+        for i in {2..15}
             do
                 # IPADDR string
                 ipaddrstring="IPADDR"
